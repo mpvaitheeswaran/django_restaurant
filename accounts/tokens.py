56667822bdbf05415b67d70a4323083633386e35
Vaitheeswaran <vaitheeswaran@buyp.com>
2022-03-30 09:25:25 +0530

    UI update.

--- accounts/tokens.py
+++ accounts/tokens.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.tokens import PasswordResetTokenGenerator
-from django.utils import six
+# from django.utils import six
+import six
 # pip install django-utils-six
 class TokenGenerator(PasswordResetTokenGenerator):
     def _make_hash_value(self, user, timestamp):
